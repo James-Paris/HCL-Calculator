ccb2c5a595948ba5e8806b38f8da5666094225a1
James <James.Paris@Hcl.com>
2020-12-22 12:14:04 -0600

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Give me a number please: ");
+		
+		System.out.println(scan.nextLine());
+		
+	}
+	
+}
