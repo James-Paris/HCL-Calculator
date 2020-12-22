0313fa9e554381d637787d354dc4513a8da6b05a
James <James.Paris@Hcl.com>
2020-12-22 12:37:25 -0600

    Added CLI for choice selection

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,44 @@
 import java.util.Scanner;
-
+/*
+ * @author: James Paris
+ * Date: 12/22/2020
+ */
 public class Calculator {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Give me a number please: ");
+		/*System.out.println("Give me a number please: "); */
 		
-		System.out.println(scan.nextLine());
+		displayOptions();
+		//String choice = scan.nextLine();
+		
+		
+		
+	}
+	
+	//This is called to give our default choice menu to select
+	//from our current features
+	public static int displayOptions() {
+		int choice;
+		do {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("");
+			System.out.println("");
+			System.out.println("###########################################################");
+			System.out.println(" [ 1 ] Addition");
+			System.out.println(" [ 2 ] Subtraction");
+			System.out.println(" [ 3 ] Multiplication");
+			System.out.println(" [ 4 ] Division");
+			System.out.println(" [ 5 ] Exit");
+			System.out.println("#################### SELECT A FUNCTION ####################");
+			System.out.println("Please select a number 1-5: ");
+			
+			//grab as a string and convert it to int, in case they entered anything besides numbers.
+			choice = Integer.parseInt(scan.nextLine());
+		} while(choice > 5 || choice < 1);
+		
+
+		return choice;
 		
 	}
 	
